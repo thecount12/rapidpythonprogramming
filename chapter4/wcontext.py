e71c90117fbefd3e1bdf037644e6389c224ad99c
William Gunnells <gunnells@gmail.com>
2020-03-06 17:28:00 -0800

    ch4 complete

--- chapter4/wcontext.py
+++ chapter4/wcontext.py
@@ -1,6 +1,16 @@
-#!/usr/bin/python
-# wcontext.py
-# Chapter 4 Files
-# Author: William C. Gunnells
-# Rapid Python
+"""
+wcontext.py
+Chapter 4 Files
+Author: William C. Gunnells
+Rapid Python
+"""
 
+
+with open('output.txt', 'w') as f:
+    f.write("Usual Hello World\nNext line is cool")
+
+with open("output.txt", 'r') as r:
+    data = r.readlines()
+
+for i in data:
+    print(i)
